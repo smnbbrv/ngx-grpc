f27724a4452f6be7a7f947db8d16f2cfb6803b70
smnbbrv <smnbbrv@gmail.com>
2020-01-10 17:41:57 +0100

    fix: properly export grpc-message

--- projects/core/src/public-api.ts
+++ projects/core/src/public-api.ts
@@ -1,6 +1,7 @@
 export * from './lib/grpc-client';
 export * from './lib/grpc-handler';
 export * from './lib/grpc-interceptor';
+export * from './lib/grpc-message';
 export * from './lib/grpc-message-class';
 export * from './lib/injection-tokens';
 
