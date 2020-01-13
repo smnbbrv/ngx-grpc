97c29fef3dc96c357e22026eb9c9dbca5cac7314
smnbbrv <smnbbrv@gmail.com>
2020-01-13 22:40:02 +0100

    fix: remove restriction of RecursivePartial for maps

--- projects/core/src/lib/utils.ts
+++ projects/core/src/lib/utils.ts
@@ -1,7 +1,3 @@
 export type RecursivePartial<T> = {
-  [P in keyof T]?:
-  T[P] extends Uint8Array ? T[P] :
-  T[P] extends (infer U)[] ? RecursivePartial<U>[] :
-  T[P] extends object ? RecursivePartial<T[P]> :
-  T[P];
+  [P in keyof T]?: T[P] extends Uint8Array ? T[P] : T[P] extends (infer U)[] ? RecursivePartial<U>[] : T[P];
 };
