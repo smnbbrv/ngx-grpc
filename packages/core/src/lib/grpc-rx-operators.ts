ed9c100910b76a634e80f23ddb66874e10cac164
smnbbrv <smnbbrv@gmail.com>
2020-02-17 19:59:08 +0100

    feat(core): add rxjs operators for better development experience with event streams

---
+++ packages/core/src/lib/grpc-rx-operators.ts
@@ -0,0 +1,26 @@
+import { GrpcDataEvent, GrpcEvent, GrpcMessage, GrpcStatusEvent } from '@ngx-grpc/common';
+import { Observable, of, throwError } from 'rxjs';
+import { filter, map, switchMap } from 'rxjs/operators';
+
+/**
+ * RxJS operator
+ * When applied to gRPC events emits error for status events with a non-zero code (includes throwStatusErrors)
+ * @return Observable of gRPC events
+ */
+export function throwStatusErrors<T extends GrpcMessage>() {
+  return (source$: Observable<GrpcEvent<T>>) => source$.pipe(
+    switchMap(event => event instanceof GrpcStatusEvent && event.code ? throwError(event) : of(event)),
+  );
+}
+
+/**
+ * RxJS operator
+ * When applied to gRPC events stream emits only messages
+ * @return Observable of messages
+ */
+export function takeMessages<T extends GrpcMessage>() {
+  return (source$: Observable<GrpcEvent<T>>) => source$.pipe(
+    filter(event => event instanceof GrpcDataEvent),
+    map((event: GrpcDataEvent<T>) => event.data),
+  );
+}
