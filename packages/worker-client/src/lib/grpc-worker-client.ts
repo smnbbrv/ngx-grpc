dbe2921e21a28b15a99b8cb0537d1f17895ffb54
Semen Bobrov <smnbbrv@gmail.com>
2020-06-08 16:02:33 +0200

    feat(worker): add global configuration support

--- packages/worker-client/src/lib/grpc-worker-client.ts
+++ packages/worker-client/src/lib/grpc-worker-client.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, Optional } from '@angular/core';
 import { GrpcClient, GrpcClientFactory, GrpcClientSettings, GrpcDataEvent, GrpcEvent, GrpcMessage, GrpcMessageClass } from '@ngx-grpc/common';
 import { Metadata } from 'grpc-web';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { GrpcWorkerGateway } from './grpc-worker-gateway';
+import { GRPC_WORKER_CLIENT_DEFAULT_SETTINGS } from './tokens';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,17 @@ import { GrpcWorkerGateway } from './grpc-worker-gateway';
 export class GrpcWorkerClientFactory implements GrpcClientFactory {
 
   constructor(
+    @Optional() @Inject(GRPC_WORKER_CLIENT_DEFAULT_SETTINGS) private defaultSettings: GrpcClientSettings,
     private gateway: GrpcWorkerGateway
   ) { }
 
-  createClient(serviceId: string, settings: GrpcClientSettings) {
+  createClient(serviceId: string, customSettings: GrpcClientSettings) {
+    const settings = customSettings || this.defaultSettings;
+
+    if (!settings) {
+      throw new Error(`Worker client factory: no settings provided for ${serviceId}`);
+    }
+
     return new GrpcWorkerClient(
       serviceId,
       { ...settings },
--- packages/worker-client/src/lib/tokens.ts
+++ packages/worker-client/src/lib/tokens.ts
@@ -1,3 +1,4 @@
 import { InjectionToken } from '@angular/core';
 
 export const GRPC_WORKER = new InjectionToken<Worker>('GRPC_WORKER');
+export const GRPC_WORKER_CLIENT_DEFAULT_SETTINGS = new InjectionToken('GRPC_WORKER_CLIENT_DEFAULT_SETTINGS');
